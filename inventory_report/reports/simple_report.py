64c91116af6fd49b5b990624569cb5c3de703d85
hector <hectoreddbenitez@gmail.com>
2022-09-21 16:46:06 -0300

    feat(req03): alguns ossinhos mais

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,10 +1,15 @@
 class SimpleReport:
     @classmethod
     def generate(self, list_):
-      ...
+
+        oldest_manufact_date = ...
+
+        next_expires_date = ...
+
+        more_products_company = ...
 
         return (
-          Data de fabricação mais antiga: YYYY-MM-DD
-          Data de validade mais próxima: YYYY-MM-DD
-          Empresa com mais produtos: NOME DA EMPRESA
+          f"Data de fabricação mais antiga: {oldest_manufact_date}"
+          f"Data de validade mais próxima: {next_expires_date}"
+          f"Empresa com mais produtos: {more_products_company}"
         )
