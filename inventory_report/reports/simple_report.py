b7f45ed62716630cb8ed4416a69cd4d52ffa0f43
hector <hectoreddbenitez@gmail.com>
2022-09-21 19:55:42 -0300

    feat(req03): Fatality!

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,15 +1,31 @@
+from datetime import date
+import statistics
+
+
 class SimpleReport:
     @classmethod
-    def generate(self, list_):
+    def generate(self, list):
 
-        oldest_manufact_date = ...
+        oldest_manufact_date = min(
+            [product["data_de_fabricacao"] for product in list]
+        )
 
-        next_expires_date = ...
+        next_expires_date = min(
+            [
+                product["data_de_validade"]
+                for product in list
+                if product["data_de_validade"] > str(date.today())
+            ]
+        )
 
-        more_products_company = ...
+        companies = []
+        for product in list:
+            companies.append(product["nome_da_empresa"])
+
+        more_products_company = statistics.mode(companies)
 
         return (
-          f"Data de fabricação mais antiga: {oldest_manufact_date}"
-          f"Data de validade mais próxima: {next_expires_date}"
-          f"Empresa com mais produtos: {more_products_company}"
+            f"Data de fabricação mais antiga: {oldest_manufact_date}\n"
+            f"Data de validade mais próxima: {next_expires_date}\n"
+            f"Empresa com mais produtos: {more_products_company}"
         )
