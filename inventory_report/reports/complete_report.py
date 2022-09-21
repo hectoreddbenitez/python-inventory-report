91614c437910f096585167d6bf7598a024d3443d
hector <hectoreddbenitez@gmail.com>
2022-09-21 20:39:43 -0300

    feat(req04)

---
+++ inventory_report/reports/complete_report.py
@@ -0,0 +1,10 @@
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    def generate(list):
+        report = SimpleReport.generate(list)
+
+        "?????????????"
+
+        return report
