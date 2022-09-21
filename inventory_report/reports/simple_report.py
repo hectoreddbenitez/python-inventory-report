18d7bdc63ff9ca9547d2fd5ea54e52bcb1e5d9fb
hector <hectoreddbenitez@gmail.com>
2022-09-21 16:38:46 -0300

    feat(req03): esqueleto da função

---
+++ inventory_report/reports/simple_report.py
@@ -0,0 +1,10 @@
+class SimpleReport:
+    @classmethod
+    def generate(self, list_):
+      ...
+
+        return (
+          Data de fabricação mais antiga: YYYY-MM-DD
+          Data de validade mais próxima: YYYY-MM-DD
+          Empresa com mais produtos: NOME DA EMPRESA
+        )
